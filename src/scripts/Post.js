5c40f8e25523d9371f53b9e923ab063b976bc049
Brandon Lisonbee <blisonbee213596@gmail.com>
2023-10-21 15:04:04 -0600

    ootststs

--- src/scripts/Post.js
+++ src/scripts/Post.js
@@ -11,12 +11,24 @@ export default class Post {
     }
 
     async init() {
+        const dateObject = new Date(this.date);
+
+        const dateOptions = { year: 'numeric', month: 'short', day: 'numeric' };
+
+        const formattedDate = dateObject.toLocaleDateString(undefined, dateOptions);
+
+        const timeParts = this.time.match(/T(\d{2}):(\d{2})/);
+        const hours = parseInt(timeParts[1]);
+        const minutes = parseInt(timeParts[2]);
+        const amOrPm = hours >= 12 ? 'PM' : 'AM';
+        const formattedTime = `${(hours % 12) || 12}:${minutes.toString().padStart(2, '0')} ${amOrPm}`;
+
         let html = `<section class="card">
             <div class="post-user">
                 <img class="profile-img" src="./public/images/placeholder.png">
                 <p>${this.name} <span>(${this.type})</span></p>
             </div>
-            <p>Date: <span>${this.date}, ${this.time}</span></p>
+            <p>Date: <span>${formattedDate}, ${formattedTime}</span></p>
             <p>Start: <span>${this.start}</span></p>
             <p>End: <span>${this.end}</span></p>
         </section>`;
